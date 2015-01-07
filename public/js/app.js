d39c2be93a6460f4cccdb91b54203a8c73915d96
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-07 11:53:46 -0500

    added jquery listener and action to de login/ singup form

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function (){
+
+$('#login').on("click", function(ev){
+  ev.preventDefault();
+  $('.login_form').toggle();
+});
+$('#singup').on("click", function(ev){
+  ev.preventDefault();
+  $('.singup_form').toggle();
+});
+
+
+
+
+
+
+
+});
