e9dab27138243feb715be3581ded75807fd4f135
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-07 18:12:56 -0500

    seting edit routs

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,5 +1,6 @@
 get '/start' do
   if current_user
+    @user = current_user.foods.all.sort_by(& :expiration_date)
     erb :start
   else
     redirect '/'
@@ -10,9 +11,26 @@ post '/new' do
   item = Food.new(params[:food])
   if item.save
     current_user.foods << item
-    redirect '/start'
+    @user = current_user.foods.all.sort_by(& :expiration_date)
+    erb :_table, :layout => false
   else
     # report problem saving data
+    redirect '/start'
+  end
+
+end
+
+get '/edit' do
+  erb :_form_edit, :layout => false
+end
+
+put '/edit' do
+  item = Food.find(params[:food][:id])
+  item.update(params[:food])
+  @user = current_user.foods.all.sort_by(& :expiration_date)
+  if request.xhr?
+    erb :_table, :layout => false
+  else
     redirect '/start'
   end
 
