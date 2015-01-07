bcee655ed5344f0830bd0622562e1d81582ddb55
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-07 11:52:22 -0500

    added login and singup routs

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,29 @@
 get '/' do
   erb :index
 end
+
+post '/login' do
+  user = User.find_by(username: params[:user][:username])
+  if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/start'
+  else
+    redirect '/'
+  end
+end
+
+post '/singup' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    redirect '/start'
+  else
+    redirect '/'
+  end
+end
+
+get '/logout' do
+ session[:user_id] =  nil
+ redirect '/'
+end
+
