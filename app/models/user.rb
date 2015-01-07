82f52fb9702e0fe8d66500db455824ee6d4c77c8
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-07 14:21:26 -0500

    added validation to user on email for precense true

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :email, uniqueness: true
+  validates :email, presence: true
   has_many :foods
 end
