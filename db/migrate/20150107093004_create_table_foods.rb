b7e2b04e341bef2f107062ad2b062754a80ad6de
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-07 14:23:41 -0500

    changes on css to table and one change on table foods migration

--- db/migrate/20150107093004_create_table_foods.rb
+++ db/migrate/20150107093004_create_table_foods.rb
@@ -2,7 +2,9 @@ class CreateTableFoods < ActiveRecord::Migration
   def change
     create_table :foods do |t|
       t.string :name
+      t.integer :amount
       t.string :expiration_date
+      t.references :user
       t.timestamps
     end
   end
